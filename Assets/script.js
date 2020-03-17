ff69bd26a77c1275eeae187a0abb8112d0208777
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-17 01:46:29 -0400

    Wrote code that displays weather icon netx to city name

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,19 @@
 // This is code for the Weather Dashboard application.
+// =========================================================
 
 console.log("hello world");
 // function that displays weather data on the browser
 function displayCityWeather(response) {
   $("#city-data > p").remove();
+  var weatherIcon = $("<img>").attr(
+    "src",
+    "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"
+  );
   $("#city-name").text(response.name);
+  $("#city-name").append(weatherIcon);
   var temp = $("<p>").text("Temperature: " + response.main.temp + "\xB0F");
   var humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
   var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
-  // var uvIndex = $('<p>').text('UV Index: ' + )
   $("#city-data").append(temp);
   $("#city-data").append(humidity);
   $("#city-data").append(windSpeed);
