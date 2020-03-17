fc8da3564d6d91a1b90416a6686176cee2c19aae
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-17 00:57:24 -0400

    Added code that grabs city data from openweather api for a city submitted in the search bar

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,18 @@
 // This is code for the Weather Dashboard application.
 
 console.log("hello world");
-//
-
-queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&";
-apiKey = "appid=32f3bd91176f3143f734153b8e41357e";
-$.ajax({
-  url: queryUrl + apiKey,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+// Grabs the data for the city that is submitted in the form
+$("#search-form").on("submit", function(event) {
+  event.preventDefault();
+  var city = $("#city-search").val();
+  baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+  var cityTerm = city + "&";
+  apiKey = "appid=32f3bd91176f3143f734153b8e41357e";
+  var queryUrl = baseUrl + cityTerm + apiKey;
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
 });
